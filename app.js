b22982c3233226d9daf553f488251d637f13acad
v-abhilash-reddy <88310034+v-abhilash-reddy@users.noreply.github.com>
2022-03-08 17:21:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+
+app.set('view engine',"ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+//connecting mongoDB
+const dbURI = 'mongodb+srv://abs1289:abs%401289@cluster0.fhm2u.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology : true}).then(console.log("connected to db")).catch((err)=>console.log(err));
+const userSchema = {
+    fullname: String,
+    cityname: String,
+    email: String,
+    password: String
+}
+const User = new mongoose.model("User",userSchema);
+
+
+//seeking routes
+app.get("/",function(req,res){
+    console.log("in the get home");
+    res.render('home');
+})
+
+app.get("/login",function(req,res){
+    console.log("in the get login");
+    res.render('login');
+})
+
+app.get("/register",function(req,res){
+    console.log("in the get register");
+    res.render('register');
+})
+
+app.post("/register",function(req,res){
+    console.log("in the post register");
+    const newUser = new User({
+        fullname: req.body.fullname,
+        cityname: req.body.cityname,
+        usermail: req.body.email,
+        password: req.body.password
+    })
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.render('i-home',{ username: userFound.fullname, city: userFound.cityname});
+        }
+    })
+})
+
+app.post("/login",function(req,res){
+    console.log("in the post login");
+    const usermail = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({usermail:usermail},function(err,userFound){
+        if(err){
+            console.log(err);
+        } else{
+            if(userFound && userFound.password===password){
+                    res.render('i-home',{ username: userFound.fullname, city: userFound.cityname});
+            } else{
+                res.render('non-user');
+            }
+        }
+    })
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server started on port 3000...");
+});
